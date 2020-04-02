b764e65a6d94a91124ddf4eba4f029285724a118
Igor Nemgirov <nimgrin@hotmail.com>
2020-04-02 18:47:33 +0300

    made some changes in app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 function Slider(selector){
     this.target = document.querySelector(selector);
     var list = this.target.querySelector('ul');
-    this.countSlides = list.children.length
+    this.countSlides = list.children.length;
     this.currentIndexSlide = 0;
-
+    
     this.target.addEventListener('click', (e)=>{
+        
         var arrow = e.target.closest('[data-vector]');
 
         if (arrow) {
@@ -20,7 +21,6 @@ function Slider(selector){
         if (this.currentIndexSlide < this.countSlides - 1){
             this.currentIndexSlide++;
             translateSlider(this.currentIndexSlide);
-        // console.log('next slider')
         }
     }
 
@@ -28,17 +28,14 @@ function Slider(selector){
         if (this.currentIndexSlide > 0){
             this.currentIndexSlide--;
             translateSlider(this.currentIndexSlide);
-        // console.log('prev slider')
         }
     }
 
     function translateSlider(index){
-        list.style.transform  = 'translateX(${-(index * 100)}%)';
+        list.style.transform = `translateX(${-(index * 100)}%)`;
     }
 }
 
 const slider1 = new Slider('#slider1', {
 
 });
-
-console.log(slider1)
