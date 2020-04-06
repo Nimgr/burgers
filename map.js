9ba427552097d00439d04e9a2cca2039db4d2805
Igor Nemgirov <nimgrin@hotmail.com>
2020-04-06 11:51:01 +0300

    added player section and yandex map

---
+++ map.js
@@ -0,0 +1,158 @@
+
+// function initMap() {
+//     var markers =  [
+//         {
+//             coords: {
+//                 "lat": 59.942378441522514,
+//                 "lng": 30.301600005249043,
+//             },
+//             addres: 'address1'
+//         }
+//     ];
+
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//         center: {
+//             lat: 59.95211426420163,
+//             lng: 30.30039837561037
+//         },
+//         zoom: 14,
+//         disableDefaultUI: true,
+//         styles: [
+//             {
+//                 "featureType": "administrative",
+//                 "elementType": "labels.text.fill",
+//                 "stylers": [
+//                     {
+//                         "color": "#444444"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "landscape",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "color": "#f2f2f2"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "poi",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "visibility": "off"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "road",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "saturation": -100
+//                     },
+//                     {
+//                         "lightness": 45
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "road.highway",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "visibility": "simplified"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "road.arterial",
+//                 "elementType": "labels.icon",
+//                 "stylers": [
+//                     {
+//                         "visibility": "off"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "transit",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "visibility": "off"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "featureType": "water",
+//                 "elementType": "all",
+//                 "stylers": [
+//                     {
+//                         "color": "#ff0000"
+//                     },
+//                     {
+//                         "visibility": "on"
+//                     }
+//                 ]
+//             }
+//         ]
+//     });
+
+//     markers.forEach((marker)=>{
+//         var infowindow = new google.maps.InfoWindow({
+//             content: marker.addres
+//         });
+
+//         var marker = new google.maps.Marker({
+//             position: marker.coords,
+//             map: map,
+//             animation: google.maps.Animation.BOUNCE,
+//             icon: './icons/map-marker.svg',
+//             title: 'hello'
+//         });
+
+//         marker.addListener('click', function() {
+//             infowindow.open(map, marker);
+//         });
+//     })
+// }
+///yandex
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [59.94237844, 30.30160000],
+
+        zoom: 11,
+        controls: []
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.8, 37.6], {
+        balloonContent: 'ADDRESS'
+    },{
+        iconLayout: 'default#image',
+        iconImageHref: './icons/map-marker.svg',
+    });
+    myMap.geoObjects.add(myPlacemark);
+}
+
+var isAnimate = false;
+
+window.addEventListener('wheel', ()=>{
+    if (isAnimate) return;
+
+    translate(index);
+})
+
+function translate(index){
+
+    isAnimate = true;
+    setTimeout(()=>{
+        isAnimate = false;
+    }, 1300)
+}
